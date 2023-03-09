150bce692914940d44c42eeecb57307b4c58e48c
Michael M Ndirangu <michaelmndirangu@gmail.com>
2023-03-09 09:06:05 +0200

    updates

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - function definition
+ * @n: the number to use
+ * Description: find factorial of n
+ * Return: factor / -1
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
